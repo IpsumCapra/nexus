f6098e943c026c3474deba7b8e298ea48c57e8f1
IpsumCapra <samuelcornelisse@gmail.com>
2021-11-04 10:00:45 +0100

    Added current plan.

--- Compiler/Compiler.cs
+++ Compiler/Compiler.cs
@@ -7,7 +7,25 @@ namespace Compiler
     {
         public static void Compile(string toCompile)
         {
+            // Split into lines of code.
             
+            // Apply PEMDAS.
+            // Solve for braces.
+            
+            // Solve functions.
+            
+            // Solve multiplications.
+            
+            // Solve divisions.
+            
+            // Solve additions.
+            
+            // Solve subtractions.
+            
+            
+            // Solve logic
+            
+            // Assign variables.
         }
     }
 }
