d1512a2f79de475930219654d5d0fc03107a153e
IpsumCapra <samuelcornelisse@gmail.com>
2021-11-04 17:41:22 +0100

    Changed variable type to const. Added NULL constant.

--- Compiler/Constants.cs
+++ Compiler/Constants.cs
@@ -3,50 +3,53 @@ namespace Compiler
     public static class Constants
     {
         // Order Control
-        public static string LINE_TERMINATOR = ";";
-        public static string ORDER_CONTROL_OPEN = "(";
-        public static string ORDER_CONTROL_CLOSE = ")";
-        public static string DEFINITION_OPEN = "{";
-        public static string DEFINITION_CLOSE = "}";
+        public const string LINE_TERMINATOR = ";";
+        public const string ORDER_CONTROL_OPEN = "(";
+        public const string ORDER_CONTROL_CLOSE = ")";
+        public const string DEFINITION_OPEN = "{";
+        public const string DEFINITION_CLOSE = "}";
         
         // Comments
-        public static string COMMENT = "//";
+        public const string COMMENT = "//";
         
         // Two Input Arithmetic Operators
-        public static string ADDITION = "+";
-        public static string SUBTRACTION = "-";
-        public static string MULTIPLICATION = "*";
-        public static string DIVISION = "/";
-        public static string MODULO = "%";
+        public const string ADDITION = "+";
+        public const string SUBTRACTION = "-";
+        public const string MULTIPLICATION = "*";
+        public const string DIVISION = "/";
+        public const string MODULO = "%";
         
         // One Input Arithmetic Operators
-        public static string AUTO_INCREMENT = "++";
-        public static string AUTO_DECREMENT = "--";
+        public const string AUTO_INCREMENT = "++";
+        public const string AUTO_DECREMENT = "--";
         
         // Assignment Operators
-        public static string EQUALS = "=";
-        public static string EQUALS_ADDITION = "+=";
-        public static string EQUALS_SUBTRACTION = "-=";
-        public static string EQUALS_MULTIPLICATION = "*=";
-        public static string EQUALS_DIVISION = "/=";
-        public static string EQUALS_MODULO = "%=";
+        public const string EQUALS = "=";
+        public const string EQUALS_ADDITION = "+=";
+        public const string EQUALS_SUBTRACTION = "-=";
+        public const string EQUALS_MULTIPLICATION = "*=";
+        public const string EQUALS_DIVISION = "/=";
+        public const string EQUALS_MODULO = "%=";
         
         // Two Input Logic Operators
-        public static string EQUALS_TO = "==";
-        public static string NOT_EQUALS_TO = "!=";
-        public static string GREATER_THAN = ">";
-        public static string GREATER_THAN_EQUAL_TO = ">=";
-        public static string LESS_THAN = "<";
-        public static string LESS_THAN_EQUAL_TO = "<=";
-        public static string AND = "&&";
-        public static string OR = "||";
+        public const string EQUALS_TO = "==";
+        public const string NOT_EQUALS_TO = "!=";
+        public const string GREATER_THAN = ">";
+        public const string GREATER_THAN_EQUAL_TO = ">=";
+        public const string LESS_THAN = "<";
+        public const string LESS_THAN_EQUAL_TO = "<=";
+        public const string AND = "&&";
+        public const string OR = "||";
         
         // One Input Logic Operators
-        public static string NOT = "!";
+        public const string NOT = "!";
         
         // Variables
-        public static string LIST_OPEN = "[";
-        public static string LIST_CLOSE = "]";
+        public const string LIST_OPEN = "[";
+        public const string LIST_CLOSE = "]";
+        
+        // Constants
+        public const string NULL = "null";
         
         // Misc
         public static string[] ENCLOSURES = {DEFINITION_OPEN, DEFINITION_CLOSE, LIST_OPEN, LIST_CLOSE};
