edf36ea32dd302f3cce99c597489df88afc22ae2
IpsumCapra <samuelcornelisse@gmail.com>
2021-11-04 14:36:00 +0100

    Created symbol splitter, brace solving.

--- Compiler/Compiler.cs
+++ Compiler/Compiler.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using static Compiler.Constants;
 
 namespace Compiler
 {
@@ -7,25 +9,134 @@ namespace Compiler
     {
         public static void Compile(string toCompile)
         {
-            // Split into lines of code.
+            //TODO: Purge comments.
             
+            // Split into lines of code, based on line termination character.
+            string[] lines = toCompile.Split(new [] {LINE_TERMINATOR}, StringSplitOptions.None);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(Solve(line));
+            }
+        }
+
+        private static bool IsGeneric(char c)
+        {
+            return char.IsLetterOrDigit(c) || c == '_';
+        }
+
+        public static string Solve(string toSolve)
+        {
+            Console.WriteLine("Solving: " + toSolve);
             // Apply PEMDAS.
             // Solve for braces.
+            if (toSolve.Contains(ORDER_CONTROL_OPEN))
+            {
+                int depth = 0;
+                int start = 0;
+                for (int i = 0; i < toSolve.Length; i++)
+                {
+                    if (toSolve[i].ToString() == ORDER_CONTROL_OPEN)
+                    {
+                        if (depth == 0)
+                            start = i;
+                        depth++;
+                    }
+
+                    if (toSolve[i].ToString() == ORDER_CONTROL_CLOSE)
+                    {
+                        depth--;
+                        if (depth == 0)
+                        {
+                            string solvedPart = Solve(toSolve.Substring(start + 1, i - start - 1));
+                            toSolve = toSolve.Remove(start, i - start + 1).Insert(start, solvedPart);
+                        }
+                    }
+                }
+
+                if (depth != 0)
+                {
+                    Console.WriteLine("Error: Uneven amount of parentheses.");
+                    return "null";
+                    //TODO: Error handling.
+                }
+            }
             
+            // Determine symbols.
+            List<String> symbols = new List<string>();
+            string currentSymbol = "";
+            for (int i = 0; i < toSolve.Length; i++)
+            {
+                char c = toSolve[i];
+                
+                bool isWhitespace = char.IsWhiteSpace(c);
+                bool isGeneric = char.IsLetterOrDigit(c) || c == '_';
+                bool isEnclosure = ENCLOSURES.Contains(c.ToString());
+
+                if ((isWhitespace || !isGeneric) && !string.IsNullOrEmpty(currentSymbol))
+                {
+                    if (!isWhitespace && !isEnclosure && !IsGeneric(currentSymbol.Last()))
+                    {
+                        currentSymbol += toSolve[i];
+                        symbols.Add(currentSymbol);
+                        currentSymbol = "";
+                        continue;
+                    }
+                    symbols.Add(currentSymbol);
+                    currentSymbol = "";
+
+                    if (isEnclosure)
+                    {
+                        symbols.Add(c.ToString());
+                    }
+                    
+                    if (c.ToString() == NOT && toSolve[i + 1].ToString() != EQUALS)
+                    {
+                        symbols.Add(c.ToString());
+                        continue;
+                    }
+                    
+                    if (c.ToString() == EQUALS && toSolve[i + 1].ToString() != EQUALS)
+                    {
+                        symbols.Add(c.ToString());
+                    }
+                }
+                else if (!isWhitespace)
+                {
+                    if (!isGeneric && (i == toSolve.Length - 1 || IsGeneric(toSolve[i + 1]) || char.IsWhiteSpace(toSolve[i + 1])))
+                    {
+                        symbols.Add(c.ToString());
+                    }
+                    else
+                    {
+                        currentSymbol += c;
+                    }
+                }
+
+            }
+
+            symbols.Add(currentSymbol);
+            foreach (string symbol in symbols)
+            {
+                Console.WriteLine(symbol);
+            }
+            Console.WriteLine("---");
+
             // Solve functions.
-            
+
             // Solve multiplications.
-            
+
             // Solve divisions.
-            
+
             // Solve additions.
-            
+
             // Solve subtractions.
-            
-            
+
+
             // Solve logic
-            
+
             // Assign variables.
+            return "null";
         }
     }
 }
