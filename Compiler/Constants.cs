8046f5ce71d4bfc52c2e48bd3237985e97f5c9f2
IpsumCapra <samuelcornelisse@gmail.com>
2021-11-04 09:52:00 +0100

    Added relevant arithmetic and logic constants.

--- Compiler/Constants.cs
+++ Compiler/Constants.cs
@@ -1,7 +1,37 @@
 namespace Compiler
 {
-    public class Constants
+    public static class Constants
     {
+        // Two Input Arithmetic Operators
+        public static string ADDITION = "+";
+        public static string SUBTRACTION = "-";
+        public static string MULTIPLICATION = "*";
+        public static string DIVISION = "/";
+        public static string MODULO = "%";
         
+        // One Input Arithmetic Operators
+        public static string AUTO_INCREMENT = "++";
+        public static string AUTO_DECREMENT = "--";
+        
+        // Assignment Operators
+        public static string EQUALS = "=";
+        public static string EQUALS_ADDITION = "+=";
+        public static string EQUALS_SUBTRACTION = "-=";
+        public static string EQUALS_MULTIPLICATION = "*=";
+        public static string EQUALS_DIVISION = "/=";
+        public static string EQUALS_MODULO = "%=";
+        
+        // Two Input Logic Operators
+        public static string EQUALS_TO = "==";
+        public static string NOT_EQUALS_TO = "!=";
+        public static string GREATER_THAN = ">";
+        public static string GREATER_THAN_EQUAL_TO = ">=";
+        public static string LESS_THAN = "<";
+        public static string LESS_THAN_EQUAL_TO = "<=";
+        public static string AND = "&&";
+        public static string OR = "||";
+        
+        // One Input Logic Operators
+        public static string NOT = "!";
     }
 }
